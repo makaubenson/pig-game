c043cbeef6199d3c9589d98d13b7f79a91957c99
unknown <bensonmakau2000@gmail.com>
2022-04-15 12:55:41 +0300

    generating random dice number

--- script.js
+++ script.js
@@ -3,8 +3,20 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //Staring conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+//rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1; // Generates random number between 1 and 6
+  //2. Display Dice
+
+  //3. Check for rolled 1 : if true switch to the next player
+});
