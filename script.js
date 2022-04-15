ce9d059e3bd5f343443a9c3a58895a47cc884c45
unknown <bensonmakau2000@gmail.com>
2022-04-15 16:19:55 +0300

    able to get current scores for each player and changer background for the active player

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
@@ -11,8 +13,11 @@ const current1El = document.getElementById('current--1');
 //Staring conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
+
+let scores = [0, 0];
 diceEl.classList.add('hidden');
 let currentScore = 0;
+let activePlayer = 0;
 
 //rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -26,8 +31,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //add dice to current score
     currentScore += dice; //currentScore = currentScore + dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //Switch to the next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0; // if active player is 0, change it to 1 else change it to 0
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
